0358635134b1b3f75683d1e84838c9e83be338bd
Steve Welburn <s.welburn@qmul.ac.uk>
2016-08-05 14:22:07 +0100

    Allow single or double-quoted values

--- clean.sh
+++ clean.sh
@@ -21,7 +21,7 @@ VARNAME_EXP="\\\$[a-zA-z0-9\\_]*api[a-zA-Z0-9\\_]*"
 API_KEY_EXP="[0-9A-F]*"
 
 # Get the API key(s) from the input
-API_KEYS=$(echo -e "$INPUT" | sed -n "s/^\(.*\)\($VARNAME_EXP\)\([ \t]*=[ \t]*\"\)\($API_KEY_EXP\)\(\".*$\)/\4/p")
+API_KEYS=$(echo -e "$INPUT" | sed -n "s/^\(.*\)\($VARNAME_EXP\)\([ \t]*=[ \t]*\)\([\'\"]\)\($API_KEY_EXP\)\(\4.*$\)/\5/p")
 for API_KEY in $API_KEYS; do
     # Encrypt the API key based on a local password file
     ENCRYPTED_KEY=$(echo $API_KEY | openssl aes-256-cbc -e -a -A -pass file:$PASSWORD_FILE)
