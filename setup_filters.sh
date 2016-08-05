773c53f8a20aa0f65fecd22ca0ddb7aa179e5784
Steve Welburn <s.welburn@qmul.ac.uk>
2016-08-05 15:24:18 +0100

    Work as a central clean and smudge rather than per project. Password files are held at project level

--- setup_filters.sh
+++ setup_filters.sh
@@ -10,22 +10,28 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 # Create removeapikey attribute
 ATTR_FILE=$GIT_ROOT/.gitattributes
 ATTR_SET=""
-if [ -e $ATTR_FILE ]; then
-    grep -v removeapikey $ATTR_FILE >$ATTR_FILE
+if [ -e "$ATTR_FILE" ]; then
+    grep -v removeapikey "$ATTR_FILE" >"$ATTR_FILE"
 fi
-echo "*.php filter=removeapikey" >>$ATTR_FILE
+echo "*.php filter=removeapikey" >>"$ATTR_FILE"
 
 # Check for existing password file
-PASSWORD_FILE="password"
+CONFIG_DIR=$GIT_ROOT/.clean_smudge
+if [ ! -d "$CONFIG_DIR" ]; then
+    mkdir "$CONFIG_DIR"
+fi
 # If no password file exists, generate a 1k random password file
-# dd if=/dev/urandom of=$PASSWORD_FILE bs=1k count=1
+PASSWORD_FILE="$CONFIG_DIR/password"
+if [ ! -e "$PASSWORD_FILE" ]; then
+    dd if=/dev/urandom of="$PASSWORD_FILE" bs=1k count=1 >/dev/null 2>&1
+fi
 
 # Ensure that the password file is ignored
-#IGN_FILE=$GIT_ROOT/.gitignore
-#if [ -e $IGN_FILE ]; then
-#    grep -v $PASSWORD_FILE $IGN_FILE >$IGN_FILE
-#fi
-#echo "$PASSWORD_FILE" >>$ATTR_FILE
+IGN_FILE=$GIT_ROOT/.gitignore
+if [ -e "$IGN_FILE" ]; then
+    grep -v ".clean_smudge" "$IGN_FILE" >"$IGN_FILE"
+fi
+echo ".clean_smudge" >>"$IGN_FILE"
 
 FILTER_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 git config filter.removeapikey.clean ${FILTER_DIR}/clean.sh
