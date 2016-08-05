e0e59623549af3b80d74f7feab6b6285f8e343e0
Steve Welburn <s.welburn@qmul.ac.uk>
2016-08-05 16:21:23 +0100

    Added framework for diff

--- setup_filters.sh
+++ setup_filters.sh
@@ -13,7 +13,7 @@ ATTR_SET=""
 if [ -e "$ATTR_FILE" ]; then
     grep -v removeapikey "$ATTR_FILE" >"$ATTR_FILE"
 fi
-echo "*.php filter=removeapikey" >>"$ATTR_FILE"
+echo "*.php filter=removeapikey diff=removeapikey" >>"$ATTR_FILE"
 
 # Check for existing password file
 CONFIG_DIR=$GIT_ROOT/.clean_smudge
@@ -36,3 +36,4 @@ echo ".clean_smudge" >>"$IGN_FILE"
 FILTER_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 git config filter.removeapikey.clean ${FILTER_DIR}/clean.sh
 git config filter.removeapikey.smudge ${FILTER_DIR}/smudge.sh
+git config diff.removeapikey.command ${FILTER_DIR}/diff.sh
