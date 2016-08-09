73cd096394973e6ec1591fdbe8d509b50736fca1
Steve Welburn <s.welburn@qmul.ac.uk>
2016-08-09 14:02:01 +0100

    Only allow = for block padding

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
 # Script for cleaning api keys in PHP scripts
 
 # Get input from STDIN or specified file name 
-INPUT=$(cat ${1:-/dev/stdin})
+INPUT=$(cat ${1:-/dev/stdin} | sed 's/\\/\\\\/g')
 
 # Specify file containing encryption/decryption password
 DIR=$(git rev-parse --show-toplevel)
@@ -19,7 +19,7 @@ fi
 function encrypt_items {
     NAME_EXP="$1"
     VALUE_EXP="$2"
-    INPUT="$3"
+    INPUT=$(echo -e "$3" | sed  -e 's/\\/\\\\/g')
 
     # Get the API key(s) from the input
     PATTERN='^\(.*\)\('"$NAME_EXP"'\)\([ \t]*=[ \t]*\)\([\'"'"'\"]\)\('"$VALUE_EXP"'\)\(\4.*$\)'
@@ -36,11 +36,11 @@ function encrypt_items {
         ESC_ENCRYPTED_ITEM=$(echo -e "$ENCRYPTED_ITEM" | sed -e 's_/_\\/_g')
         
         # Replace the API key
-        INPUT=$(echo -e "$INPUT" | sed "s/$ITEM/<<<ENCRYPTED=$ESC_ENCRYPTED_ITEM>>>/")
+        INPUT=$(echo -e "$INPUT" | sed -e "s/$ITEM/<<<ENCRYPTED=$ESC_ENCRYPTED_ITEM>>>/" -e 's/\\/\\\\/g')
     done
     # Revert to standard separator
     IFS=$OLD_IFS
-    echo -e "$INPUT"
+    echo -e "$INPUT" | sed -e 's_\\_\\\\_g'
 }
 
 # Encrypt API keys
@@ -50,12 +50,26 @@ VARNAME_EXP='\$[a-zA-z0-9\\_]*api[a-zA-Z0-9\\_]*'
 API_KEY_EXP='[0-9A-F]*'
 INPUT=$(encrypt_items "$VARNAME_EXP" "$API_KEY_EXP" "$INPUT")
 
+echo -e "$INPUT"
+exit
+
+# Encrypt email addresses
+# Regex to match a PHP variable name for an api key (has api in it)
+VARNAME_EXP='\$[a-zA-z0-9\\_]*email[a-zA-Z0-9\\_]*'
+# Regex to match a password (any characters apart from end-of-string)
+STRING_EXP='[^\4]*'
+INPUT=$(encrypt_items "$VARNAME_EXP" "$STRING_EXP" "$INPUT")
+
 # Encrypt passwords
 # Regex to match a PHP variable name for a password (has password in it)
 VARNAME_EXP="\\\$[a-zA-z0-9\\_]*password[a-zA-Z0-9\\_]*"
 # Regex to match a password (any characters apart from end-of-string)
 PASSWORD_EXP='[^\4]*'
 INPUT=$(encrypt_items "$VARNAME_EXP" "$PASSWORD_EXP" "$INPUT")
+
+# If a template file exists in the same folder as the source file check values from it
+# Start with clean_smudge
+# Ditto for filename.clean_smudge
 
 # Output the results
 echo -e "$INPUT"
